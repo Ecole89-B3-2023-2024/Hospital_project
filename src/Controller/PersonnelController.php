0874a30dab0c1c4bf83d93ec7cd795f18748a82d
login <assil782003@yahoo.com>
2024-03-02 12:11:17 +0100

    First commit

--- src/Controller/PersonnelController.php
+++ src/Controller/PersonnelController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Is_Granted('ROLE_PERSONNEL')]
+#[Is_Granted('ROLE_ADMIN')]
 #[Route('/personnel')]
 class PersonnelController extends AbstractController
 {
